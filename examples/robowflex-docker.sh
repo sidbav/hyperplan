59cbe1ceac41beb85b55f04a3dac734590349141
Sidharth Baveja <sidharthbav@gmail.com>
2022-11-12 13:10:03 -0600

    attempting to get the benchmarking to work in docker

--- examples/robowflex-docker.sh
+++ examples/robowflex-docker.sh
@@ -1,20 +1,39 @@
 #/bin/bash
-robot=${1:-fetch}
-shift
-problem=${1:-box_pick}
-shift
-opt=${1:-speed}
-shift
-testing=${1:-}
-shift
-name=${problem}-${opt}
-if [ "$testing" == "--test" ]; then
-    suffix="-test"
-fi
+#robot=${1:-fetch}
+#shift
+#problem=${1:-box_pick}
+#shift
+#opt=${1:-speed}
+#shift
+#testing=${1:-}
+#shift
+#name=${problem}-${opt}
+#if [ "$testing" == "--test" ]; then
+#    suffix="-test"
+#fi
+
+DATE=`date +"%F-%T"`
+
+SRC_MOUNT=${HOME}/Documents/hyperplan/examples
+TARGET_MOUNT=/ws_hyperplan/data
+
+# docker run \
+   #--rm \
+   #--name hyperplan \
+   #--mount type=bind,source="${SRC_MOUNT}",target="${TARGET_MOUNT}" \
+   #hyperplan ${testing} "$@" \
+   #/ws_hyperplan/data/cubicles.cfg /ws_hyperplan/data/Twistycool.cfg \
+   #>> ${HOME}/Documents/hyperplan/logs/hyperplan-${DATE}.log
+
+echo docker run \
+   --rm \
+   --name hyperplan \
+   --mount type=bind,source="${SRC_MOUNT}",target="${TARGET_MOUNT}" \
+   hyperplan ${testing} "$@"
 
 docker run \
    --rm \
-   --name ${name}${suffix} \
-   --mount type=bind,source=${HOME}/Bubox/archive/mark_moll,target=/ws_hyperplan/data \
-   hyperplan ${testing} $@ \
-   /ws_hyperplan/data/hyperplan-${robot}/${problem}-${opt}.yaml >> /tmp/hyperplan-${robot}-${problem}-${opt}${suffix}.log
+   --name hyperplan \
+   --mount type=bind,source="${SRC_MOUNT}",target="${TARGET_MOUNT}" \
+   hyperplan ${testing} "$@" \
+   >> ${HOME}/Documents/hyperplan/logs/hyperplan-${DATE}.log
